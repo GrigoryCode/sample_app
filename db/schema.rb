9dc2a61e901303b8019223cf5b2bc405ca4c05d3
GrigoryCode <grigorycode@gmail.com>
2019-01-30 20:04:40 +0300

    Users test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190127200358) do
+ActiveRecord::Schema.define(version: 2019_01_27_200358) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
